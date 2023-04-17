4f5cfabd8d58bb2c82cce6267912d62f9a84cf6b
drelocatelli <andressalocatelli@hotmail.com>
2023-04-17 11:44:37 -0300

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      transformAssetUrls: {
+        includeAbsolute: false
+      }
+    }
+  })],
   server: {
     port: 3000
   },
   preview: {
-    port: 80
+    port: 3001
   }
 })
